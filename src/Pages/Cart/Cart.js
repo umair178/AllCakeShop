6ffcdf117be51d4821705515e550532623525778
unknown <umairshabbir178@gmail.com>
2023-03-21 00:11:26 -0700

    added stripe

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,82 +1,64 @@
 import { useEffect, useRef, useState } from 'react';
 import './Cart.scss';
 import axios from 'axios';
+import Nav from '../../Components/Nav/Nav'
+import Footer from '../../Components/Footer/Footer'
 function Cart(){
     const SERVER_URL = process.env.REACT_APP_SERVER_URL;
     const formRef = useRef();
     const [cakeInCart, setCakeInCart] = useState([])
     useEffect(()=>{
         axios
-        .get(`${SERVER_URL}/cartdetails`)
+        .get(`${SERVER_URL}/cart`)
         .then(response=>{
             setCakeInCart(response.data)
             console.log('cart array is:', response.data)
 
         })
     }, []);
-
-    const addOrder = (e) => {
-        e.preventDefault();
-        // Add video to the back-end server, and then update
-        // the state with the response
-        const new_order = {
-            title: formRef.current.videotitle.value,
-            description: formRef.current.videodescription.value,
-        }
-        console.log(new_order)
-        axios.post("http://localhost:8080/videos", new_order).then((response)=>{
+    const handleclick = ()=>{ 
+        console.log('clicked')
+        const items = { items:[
+            {id: 1, quantity: 3},
+            {id: 2, quantity: 1}
+        ]}
+        axios.post(`${SERVER_URL}/create-checkout-session`, items)
+        .then(res=>{
+            console.log(res.data)
+            window.location = res.data.url
         })
-        document.getElementById('form').reset()
+        .catch(e=>{
+            console.log('error is:', e)
+        })
     };
     
     return (
         <div>
-            <h2 className="cart__title">Your Cart</h2>
-            {
-                cakeInCart.map((cake) => {
-                    return (
-                        <div className='cart__container'>
-                            <div>
-                                <img src={cake.image_url} alt='cake image isssss'></img>
+            <Nav></Nav>
+            <div className='cart__container'>
+                <h2 className="cart__title">Your Cart</h2>
+                {
+                    cakeInCart.map((cake) => {
+                        return (
+                            <div className='cart__subcontainer1'>
+                                <div className='cart__subcontainer2'>
+                                    <p className='cart__specs'>{cake.occasion}</p>
+                                    <p className='cart__specs'>Medium Serving</p>
+                                    <p className='cart__specs'>Total $80</p>
+                                </div>
+                                <div>
+                                    <img src={cake.image_url} alt='cake image isssss' className='cart__image'></img>
+                                </div>
                             </div>
-                            <div></div>
-
-                        </div>
-                    )
-                })
-            }
-    
-
-        {/* <form ref={formRef} onSubmit={addOrder} method='post' id='form'>
-            <div className='videoupload'>
-                <div className='videoupload__container1'>
-                    <p className='videoupload__thumbnail'>VIDEO THUMBNAIL</p>
-                </div>
-                <div className='upload__container'>
-                    <div>
-                        <upload className='upload__subcontainer1'>
-                            <label for='videodetitle' className='upload__label'>
-                                User ID
-                            </label>
-                            <input className='upload__input1' type='text'  name='videotitle' placeholder='Add a title to your video'></input>
-                            <label for='videodescription' className='upload__label'>
-                                ADD A VIDEO DESCRIPTION
-                            </label>
-                            <input className='upload__input2' type='text'  name='videodescription' placeholder='Add a description to your video'></input>
-                            
-                        </upload>
-                    </div>
+                        )
+                    })
+                }
+                <div className='cart__button-container' onClick={handleclick}>
+                    <button className='cart__button'>Proceed to Checkout</button>
                 </div>
             </div>
-            <div className='upload__subcontainer2'>
-                <div className='upload__publish'>
-                    <button className='upload__publish--button' >PUBLISH</button>
-                </div>
-            </div>
-        </form> */}
-            
+            <Footer></Footer>
         </div>
-
     )
 };
 export default Cart;
